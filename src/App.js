d8afca33ce520003338b58eb2a6c0ef152e12946
khalil <med.khalil.sghaier@gmail.com>
2021-12-06 19:32:04 +0100

    task manager

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import AddTask from "./Components/AddTask";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <AddTask />;
 }
 
 export default App;
---
+++ src/Components/AddTask.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const AddTask = () => {
+  let [taskName, setTaskName] = useState("");
+
+  //   Array of tasks
+  const [tasks, setTasks] = useState(["Eat", "Sleep", "Drink"]);
+
+  const onChange = (event) => {
+    setTaskName(event.target.value);
+  };
+
+  const handleAddTask = () => {
+    if (taskName.length) {
+      setTasks([...tasks, taskName]);
+    }
+  };
+
+  const removeTask = (targetTask) => {
+    setTasks(tasks.filter((task) => task !== targetTask));
+  };
+  return (
+    <div className="App">
+      <>
+        <input type="text" onChange={onChange} />
+        <button onClick={handleAddTask}>Add task!!!!</button>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task}>
+              {task}{" "}
+              <button onClick={() => removeTask(task)}>Remove Task</button>
+            </li>
+          ))}
+        </ul>
+      </>
+    </div>
+  );
+};
+
+export default AddTask;
